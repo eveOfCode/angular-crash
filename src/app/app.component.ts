77680c39ebd72059c2becd0410a3a1adc20491e0
Genevieve Twohig <eve_oftwohig@yahoo.com>
2021-07-15 10:44:43 -0300

    finished tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', // html tag to use to embed component->embedded in index.html
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash';
+  
 }
